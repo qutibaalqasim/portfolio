aee4110ee74e98d68fc2628c3b68b1b577d94a81
qutibaalqasim <qalqasim9@gmail.com>
2025-02-19 20:06:26 +0300

    starting with home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,17 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-       <div>
-        
+       <div className="wrapper flex justify-between items-center">
+        <div className=" flex-col items-center">
+          <span className="font-semibold text-base md:text-2xl text-[#707070]">Hi I am</span>
+          <h3 className="font-bold text-xl md:text-3xl text-[#959595] mt-[10px]">Qutiba Alqasim</h3>
+          <h1 className="font-black text-5xl md:text-7xl bg-gradient-to-r from-[#984300] via-[#FD6F00] to-[#CA5900] text-transparent bg-clip-text mt-6">
+            Full-Stack Developer
+          </h1>
+        </div>
+        <div className="cursor-pointer">
+            <img src="/Home/homeImg.png" alt="home img viewer" className="w-1/2 h-1/2" />
+        </div>
        </div>
     </div>
   )
