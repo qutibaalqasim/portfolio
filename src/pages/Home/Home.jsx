0de18a70128cea3c9a16baba7fa9b3970dfd70de
qutibaalqasim <qalqasim9@gmail.com>
2025-03-08 22:15:59 +0300

    make some modifications

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -493,16 +493,16 @@ export default function Home() {
     <div className=" mb-6">
       <div>
         <label htmlFor="name" className="block mb-2 text-sm font-medium text-[#959595]">name</label>
-        <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FD6F00] focus:border-[#FD6F00] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="qutiba alqasim" required />
+        <input type="text" name='name' id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FD6F00] focus:border-[#FD6F00] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="qutiba alqasim" required />
       </div>
     </div>
     <div className='mb-6'>
       <label htmlFor="email" className="block mb-2 text-sm font-medium text-[#959595]">Email address</label>
-      <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FD6F00] focus:border-[#FD6F00] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="qalqasim9@gmail.com" required />
+      <input type="email" name='email' id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FD6F00] focus:border-[#FD6F00] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="qalqasim9@gmail.com" required />
     </div> 
     <div className="mb-6">
       <label htmlFor="message" className="block mb-2 text-sm font-medium text-[#959595]">Your message</label>
-      <textarea id="message" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-[#FD6F00] focus:border-[#FD6F00] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." defaultValue={""} />
+      <textarea id="message" name='message' rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-[#FD6F00] focus:border-[#FD6F00] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." defaultValue={""} />
     </div>
     <button type="submit" className="text-white bg-[#FD6F00] hover:bg-[#984300] focus:ring-4 focus:outline-none focus:ring-[#FD6F00] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
   </form>
